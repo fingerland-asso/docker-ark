dc8cf921025b55433c914c39f3bbe9730d0a9bd6
Caderrik <caderrik@gmail.com>
2016-11-09 18:40:01 +0100

    script update

--- scripts/run-arkmanager.sh
+++ scripts/run-arkmanager.sh
@@ -3,7 +3,28 @@
 [ -p /tmp/FIFO ] && rm /tmp/FIFO
 mkfifo /tmp/FIFO
 
-function install {
+function fix-volume {
+    cd /ark
+
+    # we add user settings
+    [ ! -f "/ark/arkmanager.cfg" ] && cp /home/steam/samples/arkmanager.cfg /ark/arkmanager.cfg
+    ln -s /ark/arkmanager.cfg /home/steam/.arkmanager.cfg
+
+    # we get the instances
+    [ ! -d "/ark/instances" ] && mkdir /ark/instances
+    [ ! -f "/ark/instances/main.cfg" ] && cp /home/steam/samples/main.cfg /ark/instances
+    mkdir -p /home/steam/.config/arkmanager
+    ln -s /ark/instances /home/steam/.config/arkmanager/instances
+
+    # we fix volume tree
+    [ ! -d /ark/log ] && mkdir /ark/log
+    [ ! -d /ark/backup ] && mkdir /ark/backup
+    [ ! -d /ark/staging ] && mkdir /ark/staging
+    [ ! -L /ark/Game.ini ] && ln -s server/ShooterGame/Saved/Config/LinuxServer/Game.ini Game.ini
+    [ ! -L /ark/GameUserSettings.ini ] && ln -s server/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini GameUserSettings.ini
+}
+
+function ark-install {
     echo "No game files found. Installing..."
     mkdir -p /ark/server/ShooterGame/Saved/SavedArks
 	mkdir -p /ark/server/ShooterGame/Content/Mods
@@ -19,7 +40,7 @@ function install {
 
 function stop {
 	if [ ${BACKUPONSTOP} -eq 1 ] && [ "$(ls -A server/ShooterGame/Saved/SavedArks)" ]; then
-		echo "[Backup on stop]"
+		echo "Backuping on stop..."
 		arkmanager backup
 	fi
 	if [ ${WARNONSTOP} -eq 1 ];then
@@ -30,32 +51,20 @@ function stop {
 	exit
 }
 
-# we add user settings
-[ ! -f "/ark/arkmanager.cfg" ] && cp /home/steam/samples/arkmanager.cfg /ark/arkmanager.cfg
-ln -s /ark/arkmanager.cfg /home/steam/.arkmanager.cfg
+##################################### Main #####################################
+# We fix the volume if needed
+fix-volume
 
-# we get the instances
-[ ! -d "/ark/instances" ] && mkdir /ark/instances
-[ ! -f "/ark/instances/main.cfg" ] && cp /home/steam/samples/main.cfg /ark/instances
-mkdir -p /home/steam/.config/arkmanager
-ln -s /ark/instances /home/steam/.config/arkmanager/instances
+# We check if the game need to be installed
+[ ! -d /ark/server ] || [ ! -f /ark/server/arkversion ] && ark-install
 
-# we fix volume tree
-cd /ark
-[ ! -d /ark/log ] && mkdir /ark/log
-[ ! -d /ark/backup ] && mkdir /ark/backup
-[ ! -d /ark/staging ] && mkdir /ark/staging
-[ ! -L /ark/Game.ini ] && ln -s server/ShooterGame/Saved/Config/LinuxServer/Game.ini Game.ini
-[ ! -L /ark/GameUserSettings.ini ] && ln -s server/ShooterGame/Saved/Config/LinuxServer/GameUserSettings.ini GameUserSettings.ini
-
-# we check if the game is already installed
-[ ! -d /ark/server ] || [ ! -f /ark/server/arkversion ] && install
-
+# Backup on stat is unable ?
 if [ ${BACKUPONSTART} -eq 1 ] && [ "$(ls -A server/ShooterGame/Saved/SavedArks/)" ]; then
-    echo "[Backup]"
+    echo "Backuping..."
     arkmanager backup
 fi
 
+# Server
 if [ ${UPDATEONSTART} -eq 0 ]; then
     arkmanager start -noautoupdate
 else
